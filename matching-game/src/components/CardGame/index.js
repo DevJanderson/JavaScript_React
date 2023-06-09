48e53ec3d69f0664fbb2fa13c012981d4c01b909
Janderson Lira <devjanderlira@gmail.com>
2023-06-09 14:09:01 -0400

    settings base do project

---
+++ matching-game/src/components/CardGame/index.js
@@ -0,0 +1,11 @@
+import "./style.css"
+
+function CardGame() {
+  return `
+    <article class="card-game">
+      <img src="img/alura-pixel.png" alt="logo da alura">
+    </article>
+  `
+}
+
+export default CardGame;
