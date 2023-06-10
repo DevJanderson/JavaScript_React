87e2aeb70da500c23ec55f8dc721edc177826745
Janderson Lira <devjanderlira@gmail.com>
2023-06-10 16:33:51 -0400

    :construction: Component boardcard
    upload component

--- matching-game/main.js
+++ matching-game/main.js
@@ -1,13 +1,9 @@
-import './src/styles/setting/colors.css';
-import './src/styles/generic/reset.css';
-import './src/styles/elements/base.css';
+import "./src/styles/settings/colors.css";
+import "./src/styles/generic/reset.css";
+import "./src/styles/elements/base.css";
+import BoardGame from "./src/objects/BoardGame";
 
+const $root = document.querySelector("#root");
+const $htmlBoardGame = BoardGame(4);
 
-import CardGame  from "./src/components/CardGame";
-
-const $root = document.querySelector('#root');
-const $htmlCardGame = CardGame();
-
-console.log ($htmlCardGame)
-
-$root.insertAdjacentHTML ("beforeend", $htmlCardGame );
+$root.insertAdjacentHTML("beforeend", $htmlBoardGame);
